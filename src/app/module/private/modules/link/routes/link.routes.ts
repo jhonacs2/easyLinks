c44ebf6926f7a88d53996ba87f1cbb796840787d
Jhonatan Soto <jhonrocker2012@gmail.com>
2023-12-26 16:48:43 -0400

    created structure to create links

--- src/app/module/private/modules/link/routes/link.routes.ts
+++ src/app/module/private/modules/link/routes/link.routes.ts
@@ -1,9 +1,16 @@
 import {Routes} from '@angular/router';
 import {MainLinkViewComponent} from '../view/main-link-view/main-link-view.component';
+import {CreateLinkComponent} from '../view/create-link/create-link.component';
 
 export const LINK_ROUTES: Routes = [
   {
     path: '',
-    component: MainLinkViewComponent
+    component: MainLinkViewComponent,
+    children: [
+      {
+        path: 'create',
+        component: CreateLinkComponent
+      }
+    ]
   }
 ];
