8377aeb2e01b3fc94c1b733c75a27290f57ffd71
Jhonatan Soto <jhonrocker2012@gmail.com>
2024-01-13 22:51:19 -0400

    added upload image and change background color

--- src/app/module/private/modules/link/view/profile/profile.component.ts
+++ src/app/module/private/modules/link/view/profile/profile.component.ts
@@ -1,4 +1,4 @@
-import {Component} from '@angular/core';
+import {Component, ElementRef, Renderer2, ViewChild} from '@angular/core';
 
 @Component({
   selector: 'el-profile',
@@ -6,8 +6,33 @@ import {Component} from '@angular/core';
   styleUrl: './profile.component.scss'
 })
 export class ProfileComponent {
+  @ViewChild('imageInput') public imageInput!: ElementRef<HTMLInputElement>;
+  @ViewChild('containerImageInput') public containerImageInput!: ElementRef<HTMLDivElement>;
 
-  uploadImage(imageInput: HTMLInputElement): void {
-    imageInput.click();
+  isImageUpload: boolean;
+
+  constructor(private _renderTwo: Renderer2) {
+    this.isImageUpload = false;
+  }
+
+  uploadImage(): void {
+    this.imageInput.nativeElement.click();
+  }
+
+  loadImage(): void {
+    if (this.imageInput.nativeElement.files!.length > 0) {
+      const urlImage = window.URL.createObjectURL(this.imageInput.nativeElement.files![0]);
+      this._setBackgroundImage(urlImage);
+      this._activeInputImage();
+      this.isImageUpload = true;
+    }
+  }
+
+  private _setBackgroundImage(urlImage: string): void {
+    this._renderTwo.setStyle(this.containerImageInput.nativeElement, 'background-image', 'url(\'' + urlImage + '\')');
+  }
+
+  private _activeInputImage(): void {
+    this._renderTwo.addClass(this.containerImageInput.nativeElement, 'el-profile-container__image-input--active');
   }
 }
