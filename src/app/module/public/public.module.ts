0d78f7cdfdc6f62294beccdd0c6d14e838bd1826
Jhonatan Soto <jhonrocker2012@gmail.com>
2024-01-22 21:12:31 -0400

    feat: added login ui

--- src/app/module/public/public.module.ts
+++ src/app/module/public/public.module.ts
@@ -2,10 +2,12 @@ import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {PublicViewComponent} from './views/public-view/public-view.component';
 import {PublicRoutingModule} from './routes/public-routing.module';
+import { LoginComponent } from './components/login/login.component';
 
 @NgModule({
   declarations: [
-    PublicViewComponent
+    PublicViewComponent,
+    LoginComponent
   ],
   imports: [
     CommonModule,
--- src/app/module/public/routes/public.routes.ts
+++ src/app/module/public/routes/public.routes.ts
@@ -1,10 +1,21 @@
 import {Routes} from '@angular/router';
 import {PublicViewComponent} from '../views/public-view/public-view.component';
+import {LoginComponent} from '../components/login/login.component';
 
 export const PUBLIC_ROUTES: Routes = [
   {
     path: '',
     component: PublicViewComponent,
-    children: []
+    children: [
+      {
+        path: 'login',
+        component: LoginComponent
+      }
+    ]
+  },
+  {
+    path: '',
+    redirectTo: 'login',
+    pathMatch: 'full'
   }
 ];
