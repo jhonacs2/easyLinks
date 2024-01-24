55208ca3d892a2397bf38cd32a3791b2e63b3f94
Jhonatan Soto <jhonrocker2012@gmail.com>
2024-01-23 21:38:16 -0400

    feat: added register ui

---
+++ src/app/module/public/components/register/register.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'el-register',
+  templateUrl: './register.component.html',
+  styleUrl: './register.component.scss'
+})
+export class RegisterComponent {
+
+}
--- src/app/module/public/public.module.ts
+++ src/app/module/public/public.module.ts
@@ -3,11 +3,13 @@ import {CommonModule} from '@angular/common';
 import {PublicViewComponent} from './views/public-view/public-view.component';
 import {PublicRoutingModule} from './routes/public-routing.module';
 import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
 
 @NgModule({
   declarations: [
     PublicViewComponent,
-    LoginComponent
+    LoginComponent,
+    RegisterComponent
   ],
   imports: [
     CommonModule,
